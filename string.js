05c8fc8c9416d9ca79d9fa78537e21af7d8e4a67
meheraz1100 <meheraz1100@gmail.com>
2024-02-07 08:14:10 +0600

    basic string

---
+++ string.js
@@ -0,0 +1,31 @@
+const country = 'Bangladesh';
+const division = 'Chottogram';
+const district = 'Feni';
+const thana = new String('Feni');
+
+
+// console.log(typeof country);
+// console.log(typeof division);
+// console.log(typeof district);
+// console.log(typeof thana);
+// console.log(thana);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7,];
+// console.log(numbers.length);
+// console.log(numbers[3]);
+numbers[1] = 11;
+// console.log(numbers);
+
+
+
+const capital = 'Dhaka';
+
+// console.log(capital.length);
+// console.log(capital[2]);
+
+console.log(capital);
+capital[0] = 'F';
+console.log(capital);
+
+//string is immutable --> not changeable
+// array is mutable --> you can change the array elements
